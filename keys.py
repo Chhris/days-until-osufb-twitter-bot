b6f26cc45665589e630aa3f40cb5088b81a345b4
Chris Emmett <emm.chris7@gmail.com>
2023-01-08 19:27:18 -0500

    Create keys.py
    uploaded file for Twitter API Keys without sensitive information

---
+++ keys.py
@@ -0,0 +1,6 @@
+# Credentials to authenticate with Twitter API V2
+api_key = "Your API Key"
+api_secret = "Your API Secret"
+bearer_token = r"Your Bearer Token"
+access_token = "Your Access Token"
+access_token_secret = "Your Access Token Secret"
